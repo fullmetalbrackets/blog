abc1f6c53a6a9ae2319e7c532bd24ff49a2f1158
Ariel <fullmetalbrackets@gmail.com>
2022-09-29 14:06:34 -0400

    add prefetching for blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://arieldiaz.codes',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		syntaxHighlight: 'prism',
-}});
+  site: 'https://arieldiaz.codes',
+  integrations: [mdx(), sitemap(), prefetch(
+    {
+      throttle: 3
+    }
+  )],
+  markdown: {
+    syntaxHighlight: 'prism'
+  }
+});
