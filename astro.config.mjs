223cab18275ab049db6b4528b1c9152074a5c2ff
Ariel <fullmetalbrackets@gmail.com>
2022-11-11 01:13:51 -0500

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
