8ac043a0f042781ef718c1caa796c73a8a551565
--replace-all <fullmetalbrackets@gmail.com>
2023-10-27 21:01:10 -0400

    Merge branch 'rebuild' of C:/Users/Ariel/Desktop/Projects/blog

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -198,13 +198,412 @@ declare module 'astro:content' {
 	>;
 
 	type ContentEntryMap = {
-		
+		"blog": {
+"5-ways-to-host-site-free.md": {
+	id: "5-ways-to-host-site-free.md";
+  slug: "5-ways-to-host-site-free";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"How-to-generate-GPG-to-sign-git-commits.md": {
+	id: "How-to-generate-GPG-to-sign-git-commits.md";
+  slug: "how-to-generate-gpg-to-sign-git-commits";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"autologin-ubuntu-on-reboot.md": {
+	id: "autologin-ubuntu-on-reboot.md";
+  slug: "autologin-ubuntu-on-reboot";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"basic-linux-commands.md": {
+	id: "basic-linux-commands.md";
+  slug: "basic-linux-commands";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"bootstrapping-fresh-install-with-ansible.md": {
+	id: "bootstrapping-fresh-install-with-ansible.md";
+  slug: "bootstrapping-fresh-install-with-ansible";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"copy-ssh-keys-between-hosts.md": {
+	id: "copy-ssh-keys-between-hosts.md";
+  slug: "copy-ssh-keys-between-hosts";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"create-public-samba-share-without-login.md": {
+	id: "create-public-samba-share-without-login.md";
+  slug: "create-public-samba-share-without-login";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"customizing-windows-terminal-with-ohmyposh.md": {
+	id: "customizing-windows-terminal-with-ohmyposh.md";
+  slug: "customizing-windows-terminal-with-ohmyposh";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"dns-over-https-backends.md": {
+	id: "dns-over-https-backends.md";
+  slug: "dns-over-https-backends";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"formatting-on-linux.md": {
+	id: "formatting-on-linux.md";
+  slug: "formatting-on-linux";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"generating-an-ssh-key-pair.md": {
+	id: "generating-an-ssh-key-pair.md";
+  slug: "generating-an-ssh-key-pair";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"git-commands-cheat-sheet.md": {
+	id: "git-commands-cheat-sheet.md";
+  slug: "git-commands-cheat-sheet";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"guide-to-zsh-ohmyzsh-plugins-and-theme.md": {
+	id: "guide-to-zsh-ohmyzsh-plugins-and-theme.md";
+  slug: "guide-to-zsh-ohmyzsh-plugins-and-theme";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"host-a-static-website-with-aws-amplify.md": {
+	id: "host-a-static-website-with-aws-amplify.md";
+  slug: "host-a-static-website-with-aws-amplify";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"how-to-format-partiton-linux.md": {
+	id: "how-to-format-partiton-linux.md";
+  slug: "how-to-format-partiton-linux";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"how-to-run-filebrowser-in-docker.md": {
+	id: "how-to-run-filebrowser-in-docker.md";
+  slug: "how-to-run-filebrowser-in-docker";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"how-to-use-a-custom-prismjs-theme-with-nuxt-content.md": {
+	id: "how-to-use-a-custom-prismjs-theme-with-nuxt-content.md";
+  slug: "how-to-use-a-custom-prismjs-theme-with-nuxt-content";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"how-to-use-netlify-forms-in-a-nuxt-site.md": {
+	id: "how-to-use-netlify-forms-in-a-nuxt-site.md";
+  slug: "how-to-use-netlify-forms-in-a-nuxt-site";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"install-and-use-sudo-debian.md": {
+	id: "install-and-use-sudo-debian.md";
+  slug: "install-and-use-sudo-debian";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"install-docker-debian-ubuntu.md": {
+	id: "install-docker-debian-ubuntu.md";
+  slug: "install-docker-debian-ubuntu";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"install-latest-nodejs-debian.md": {
+	id: "install-latest-nodejs-debian.md";
+  slug: "install-latest-nodejs-debian";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"install-nodejs-ubuntu-debian.md": {
+	id: "install-nodejs-ubuntu-debian.md";
+  slug: "install-nodejs-ubuntu-debian";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"keeping-a-linux-laptop-on-with-the-lid-closed.md": {
+	id: "keeping-a-linux-laptop-on-with-the-lid-closed.md";
+  slug: "keeping-a-linux-laptop-on-with-the-lid-closed";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"markdown-hacks.md": {
+	id: "markdown-hacks.md";
+  slug: "markdown-hacks";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"minimal-blog-deno-deploy.md": {
+	id: "minimal-blog-deno-deploy.md";
+  slug: "minimal-blog-deno-deploy";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"mounting-hard-drives-in-linux.md": {
+	id: "mounting-hard-drives-in-linux.md";
+  slug: "mounting-hard-drives-in-linux";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"openmediavault-quick-reference.md": {
+	id: "openmediavault-quick-reference.md";
+  slug: "openmediavault-quick-reference";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"pi-hole-quad9-dls-over-tls.md": {
+	id: "pi-hole-quad9-dls-over-tls.md";
+  slug: "pi-hole-quad9-dls-over-tls";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"pihole-rate-limiting.md": {
+	id: "pihole-rate-limiting.md";
+  slug: "pihole-rate-limiting";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"powershell-commands-cheat-cheat.md": {
+	id: "powershell-commands-cheat-cheat.md";
+  slug: "powershell-commands-cheat-cheat";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"reverse-proxy-nginx-pihole.md": {
+	id: "reverse-proxy-nginx-pihole.md";
+  slug: "reverse-proxy-nginx-pihole";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"rsync-a-quick-guide.md": {
+	id: "rsync-a-quick-guide.md";
+  slug: "rsync-a-quick-guide";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"set-static-ip-debian.md": {
+	id: "set-static-ip-debian.md";
+  slug: "set-static-ip-debian";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"set-up-pihole-on-linux.md": {
+	id: "set-up-pihole-on-linux.md";
+  slug: "set-up-pihole-on-linux";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setting-up-a-container-stack-with-docker-compose.md": {
+	id: "setting-up-a-container-stack-with-docker-compose.md";
+  slug: "setting-up-a-container-stack-with-docker-compose";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setting-up-and-configuring-nuxt-sitemap-module-in-a-nuxt-content-blog.md": {
+	id: "setting-up-and-configuring-nuxt-sitemap-module-in-a-nuxt-content-blog.md";
+  slug: "setting-up-and-configuring-nuxt-sitemap-module-in-a-nuxt-content-blog";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setting-up-jellyfin-in-docker.md": {
+	id: "setting-up-jellyfin-in-docker.md";
+  slug: "setting-up-jellyfin-in-docker";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setting-up-plex-in-docker.md": {
+	id: "setting-up-plex-in-docker.md";
+  slug: "setting-up-plex-in-docker";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setup-a-samba-share-on-linux-via-command-line.md": {
+	id: "setup-a-samba-share-on-linux-via-command-line.md";
+  slug: "setup-a-samba-share-on-linux-via-command-line";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setup-cloudflare-tunnel-to-access-self-hosted-apps.md": {
+	id: "setup-cloudflare-tunnel-to-access-self-hosted-apps.md";
+  slug: "setup-cloudflare-tunnel-to-access-self-hosted-apps";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setup-nfs-shares-linux.md": {
+	id: "setup-nfs-shares-linux.md";
+  slug: "setup-nfs-shares-linux";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setup-photosync-with-smb-server.md": {
+	id: "setup-photosync-with-smb-server.md";
+  slug: "setup-photosync-with-smb-server";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setup-prometheus-cadvisor-grafana.md": {
+	id: "setup-prometheus-cadvisor-grafana.md";
+  slug: "setup-prometheus-cadvisor-grafana";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setup-ssh-authentication-to-push-to-github.md": {
+	id: "setup-ssh-authentication-to-push-to-github.md";
+  slug: "setup-ssh-authentication-to-push-to-github";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"setup-unattended-upgrades.md": {
+	id: "setup-unattended-upgrades.md";
+  slug: "setup-unattended-upgrades";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"sideload-the-directv-stream-app-on-sony-bravia-tv-with-android-tv.md": {
+	id: "sideload-the-directv-stream-app-on-sony-bravia-tv-with-android-tv.md";
+  slug: "sideload-the-directv-stream-app-on-sony-bravia-tv-with-android-tv";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"solid-explorer-samba-share.md": {
+	id: "solid-explorer-samba-share.md";
+  slug: "solid-explorer-samba-share";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"styling-the-kbd-element.md": {
+	id: "styling-the-kbd-element.md";
+  slug: "styling-the-kbd-element";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"sudo-without-password.md": {
+	id: "sudo-without-password.md";
+  slug: "sudo-without-password";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"sync-bare-metal-pihole-with-container.md": {
+	id: "sync-bare-metal-pihole-with-container.md";
+  slug: "sync-bare-metal-pihole-with-container";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"transferring-files-between-hosts-with-scp.md": {
+	id: "transferring-files-between-hosts-with-scp.md";
+  slug: "transferring-files-between-hosts-with-scp";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"turn-off-display-linux-terminal.md": {
+	id: "turn-off-display-linux-terminal.md";
+  slug: "turn-off-display-linux-terminal";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"turn-static-website-into-pwa.md": {
+	id: "turn-static-website-into-pwa.md";
+  slug: "turn-static-website-into-pwa";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"using-dns-over-https-with-pihole.md": {
+	id: "using-dns-over-https-with-pihole.md";
+  slug: "using-dns-over-https-with-pihole";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"using-prismjs-in-a-nuxt-static-site.md": {
+	id: "using-prismjs-in-a-nuxt-static-site.md";
+  slug: "using-prismjs-in-a-nuxt-static-site";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"using-surge-sh-to-host-static-sites.md": {
+	id: "using-surge-sh-to-host-static-sites.md";
+  slug: "using-surge-sh-to-host-static-sites";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+"vim-quick-guide.md": {
+	id: "vim-quick-guide.md";
+  slug: "vim-quick-guide";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".md"] };
+};
+
 	};
 
 	type DataEntryMap = {
-		"blog": {
-};
-"links": {
+		"links": {
 "chmod-calc": {
 	id: "chmod-calc";
   collection: "links";
