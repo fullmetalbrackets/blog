ca5fbe5a4c0dd326b349695bdb954276892fada4
--replace-all <fullmetalbrackets@gmail.com>
2024-02-27 18:07:27 -0500

    migrate from astrojs/prefetch

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fullmetalbrackets.com',
-  integrations: [mdx(), sitemap(), prefetch(
-    {
-      throttle: 10
-    }
-  )],
+  integrations: [mdx(), sitemap()],
+  prefetch: true,
   markdown: {
     drafts: false,
     syntaxHighlight: 'prism'
