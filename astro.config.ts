e02d5e072c9914eaf1bd62c6dae402746775f780
--replace-all <fullmetalbrackets@gmail.com>
2024-03-08 23:54:22 -0500

    add mode: directory to cloudflare adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fullmetalbrackets.com',
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    imageService: 'passthrough'
-  }),
+  adapter: cloudflare({ mode: "directory" },
+  { imageService: 'passthrough' }),
   output: 'server',
   prefetch: true,
   markdown: {
