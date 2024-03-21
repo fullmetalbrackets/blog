f94b9804f387f0d7f0ab9a8f5a0a58bbe9f01a77
Ariel Diaz <fullmetalbrackets@gmail.com>
2024-03-21 19:44:23 -0400

    test without cloudflare adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fullmetalbrackets.com',
   integrations: [mdx(), sitemap()],
-  output: 'hybrid',
-  adapter: cloudflare(),
+  // output: 'hybrid',
+  // adapter: cloudflare(),
   image: {
     service: {
       entrypoint: "./src/services/passThroughImageService",
     }},
   prefetch: true,
   markdown: {
-    drafts: false,
     syntaxHighlight: 'prism'
   },
   compressHTML: true
