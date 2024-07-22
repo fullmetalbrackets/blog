52e1b7c1be834c84ac5ded1786a3e548809eeeaf
Ariel Diaz <fullmetalbrackets@gmail.com>
2024-07-21 21:52:26 -0400

    update types

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -837,5 +837,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("../src/content/config.js");
+	export type ContentConfig = typeof import("./../src/content/config.js");
 }
