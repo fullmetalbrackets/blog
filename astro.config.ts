180e71f2b6f4abfe6abd9c5ce45da09052d2e38f
Ariel Diaz <fullmetalbrackets@gmail.com>
2025-02-06 03:17:14 -0500

    using astro's new external redirect feature

--- astro.config.ts
+++ astro.config.ts
@@ -13,5 +13,35 @@ export default defineConfig({
   compressHTML: true,
   image: {
     service: passthroughImageService()
-  }
+  },
+  redirects: {
+    '/feed': {
+      status: 302,
+      destination: '/rss.xml',
+    },
+    '/mail': {
+      status: 302,
+      destination: 'mailto:contact@fullmetalbrackets.com',
+    },
+    '/github': {
+      status: 302,
+      destination: 'https://github.com/fullmetalbrackets',
+    },
+    '/bsky': {
+      status: 302,
+      destination: 'https://bsky.app/profile/fullmetalbrackets.bsky.social',
+    },
+    '/social': {
+      status: 302,
+      destination: 'https://social.lol/@adiaz',
+    },
+    '/donate': {
+      status: 302,
+      destination: 'https://buymeacoffee.com/arieldiaz',
+    },
+    '/umami': {
+      status: 302,
+      destination: 'https://cloud.umami.is/share/5MdOgBcRzVP6FU0x/fullmetalbrackets.com',
+    },
+  },
 });
