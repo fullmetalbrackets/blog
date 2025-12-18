f0b9a6813268517ebdf7d059bb84124e472d0411
Ariel Diaz <fullmetalbrackets@gmail.com>
2025-12-17 20:44:52 -0500

    move to utils directory & refactor

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -1,8 +1,18 @@
 import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
+import type { Root } from 'mdast';
+import { VFile } from 'vfile';
 
 export function remarkReadingTime() {
-  return function (tree, file) {
+  interface AstroData {
+    frontmatter: Record<string, unknown>;
+  }
+
+  interface FileData {
+    astro: AstroData;
+  }
+
+  return function (tree: Root, file: VFile & { data: FileData }): void {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
     
