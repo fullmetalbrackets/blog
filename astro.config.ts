fba7a3908ed778cb10deaef1ba1dc858f7ba2bce
Ariel Diaz <fullmetalbrackets@gmail.com>
2025-12-24 22:49:09 -0500

    remove some redirects & change 301s to 302s

--- astro.config.ts
+++ astro.config.ts
@@ -31,30 +31,26 @@ export default defineConfig({
   },
   redirects: {
     '/feed': {
-      status: 301,
+      status: 302,
       destination: '/rss.xml',
     },
-    // '/mail': {
-    //   status: 301,
-    //   destination: 'mailto:contact@fullmetalbrackets.com',
-    // },
-    '/github': {
+    '/github/': {
       status: 302,
       destination: 'https://github.com/fullmetalbrackets',
     },
-    '/bsky': {
+    '/bsky/': {
       status: 302,
       destination: 'https://bsky.app/profile/fullmetalbrackets.bsky.social',
     },
-    '/social': {
-      status: 301,
+    '/social/': {
+      status: 302,
       destination: 'https://social.lol/@adiaz',
     },
-    '/donate': {
-      status: 301,
+    '/donate/': {
+      status: 302,
       destination: 'https://buymeacoffee.com/arieldiaz',
     },
-    '/umami': {
+    '/umami/': {
       status: 302,
       destination: 'https://cloud.umami.is/share/5MdOgBcRzVP6FU0x/fullmetalbrackets.com',
     },
@@ -62,50 +58,46 @@ export default defineConfig({
       status: 302,
       destination: '/blog/',
     },
-    '/blog/tailscale': {
-      status: 301,
+    '/blog/tailscale/': {
+      status: 302,
       destination: '/blog/comprehensive-guide-tailscale-securely-access-home-network/',
     },
     '/blog/factory-restore-zimaboardmd/': {
-      status: 301,
+      status: 302,
       destination: '/blog/factory-restore-zimaboard/',
     },
     '/blog/expose-plex-tailscale-vps)': {
-      status: 301,
+      status: 302,
       destination: '/blog/expose-plex-tailscale-vps/',  
     },
     '/blog/expose-plex-tailscale-vps/)': {
-      status: 301,
+      status: 302,
       destination: '/blog/expose-plex-tailscale-vps/',  
     },
     '/blog/how-to-use-sudo-without-a-password/': {
-      status: 301,
+      status: 302,
       destination: '/blog/sudo-without-password/',
     },
-    '/tailscale': {
-      status: 301,
+    '/tailscale/': {
+      status: 302,
       destination: '/blog/comprehensive-guide-tailscale-securely-access-home-network/',
     },
-    '/mergerfs': {
-      status: 301,
+    '/mergerfs/': {
+      status: 302,
       destination: '/blog/two-drives-mergerfs/',
     },
-    '/tunnel': {
-      status: 301,
+    '/tunnel/': {
+      status: 302,
       destination: '/blog/setup-cloudflare-tunnel-to-access-self-hosted-apps/',
     },
-    '/plex-tailscale': {
-      status: 301,
+    '/plex-tailscale/': {
+      status: 302,
       destination: '/blog/expose-plex-tailscale-vps/',
     },
-    '/reverse-proxy': {
-      status: 301,
+    '/reverse-proxy/': {
+      status: 302,
       destination: '/blog/reverse-proxy-using-nginx-pihole-cloudflare/',
     },
-    '/uses': {
-      status: 301,
-      destination: '/wiki/uses/',
-    }
   },
     experimental: {
     fonts: [
