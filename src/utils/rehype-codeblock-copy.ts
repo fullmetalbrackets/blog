cc79fa8e554c30e8989e852a8e2b5fe466640393
Ariel Diaz <fullmetalbrackets@gmail.com>
2026-01-20 21:17:20 -0500

    change appearance of copy button

--- src/utils/rehype-codeblock-copy.ts
+++ src/utils/rehype-codeblock-copy.ts
@@ -7,7 +7,20 @@ export default function rehypeCopyButton() {
       if (node.tagName === "pre" && node.children[0]?.tagName === "code" && index !== undefined) {
         const wrapper = h("div", { class: "code-wrapper" }, [
           node,
-          h("button", { class: "copy-btn", type: "button" }, "Copy"),
+          h("button", { class: "copy-btn", type: "button" }, [
+            h("svg", { 
+              xmlns: "http://www.w3.org/2000/svg", 
+              width: "16", 
+              height: "16", 
+              viewBox: "0 0 24 24",
+              fill: "none",
+              stroke: "currentColor",
+              strokeWidth: "2"
+            }, [
+              h("rect", { x: "9", y: "9", width: "13", height: "13", rx: "2", ry: "2" }),
+              h("path", { d: "M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" })
+            ])
+          ]),
         ]);
         parent.children[index] = wrapper;
       }
