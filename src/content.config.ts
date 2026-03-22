9848b2ae0200c61c7cacc44a3ce080b6b8451a3c
Ariel Diaz <fullmetalbrackets@gmail.com>
2026-03-22 01:41:56 -0400

    update lifestream & delete unused digest

--- src/content.config.ts
+++ src/content.config.ts
@@ -43,78 +43,17 @@ const wiki = defineCollection({
 });
 
 const lifestream = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.yml', base: './src/content/lifestream' }),
+	loader: glob({ pattern: '**/[^_]*.md', base: './src/content/lifestream' }),
 	schema: ({ image }) =>
 		z.object({
 			// required
-			type: z.enum([
-				'movie',
-				'tvshow',
-				'book',
-				'game',
-				'quote',
-				'note',
-				'youtube',
-				'image',
-			]),
-			title: z.string(),
-			pubDate: z.coerce.date(),
-			// common fields
-			description: z.string().optional(),
-			url: z.url().optional(),
-			note: z.string().optional(),
-			startDate: z.date().optional(),
-			endDate: z.date().optional(),
-			genre: z.array(z.string()).optional(),
-			publisher: z.string().optional(),
-			platform: z.array(z.string()).optional(),
-			image: image().optional(),
-			dbid: z.number().optional(),
-			// notes
-			tag: z.string().optional(),
-			// movies
-			director: z.string().optional(),
-			stars: z.array(z.string()).optional(),
-			// books
-			author: z.string().optional(),
-			isbn: z.number().optional(),
-			// games
-			developer: z.string().optional(),
-			completed: z.boolean().default(true),
-			steamdb: z.number().optional(),
-			// tvshows
-			inProgress: z.boolean().optional().default(false),
-			current: z.boolean().optional(),
-			seasons: z.number().max(99).optional(),
-			// youtube
-			videoId: z.string().optional(),
-			// quotes
-			quote: z.string().optional(),
-			attribution: z.string().optional(),
-		}),
-});
-
-const digest = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.md', base: './src/content/digest' }),
-	schema: ({ image }) =>
-		z.object({
-			// required
-			type: z.enum(['movie', 'tvshow', 'game', 'book']),
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
+			type: z.enum(['movie', 'tvshow', 'book', 'game']),
+			rating: z.enum(['hated', 'disliked', 'liked', 'loved', 'masterpiece']),
 			image: image(),
-			// movies & tvshows
-			tmdb: z.number().optional(),
-			// tvshows
-			season: z.number().max(15).optional(),
-			// books
-			isbn: z.number().optional(),
-			// games
+			title: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
 			platform: z.string().optional(),
-			completed: z.boolean().optional(),
-			steamdb: z.number().optional(),
-			moby: z.number().optional(),
 		}),
 });
 
@@ -150,7 +89,6 @@ export const collections = {
 	links,
 	wiki,
 	lifestream,
-	digest,
 	games,
 	now,
 };
